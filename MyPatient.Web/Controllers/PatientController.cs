a2a222fa69e6d978774eb90ffb021c0b66123d76
EstivenPad <estivenps26@gmail.com>
2024-06-06 16:21:18 -0400

    feat(patient): adding PatientController

---
+++ MyPatient.Web/Controllers/PatientController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPatient.Web.Models;
+
+namespace MyPatient.Web.Controllers
+{
+    public class PatientController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Patient patient)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View(patient);
+        }
+    }
+}
