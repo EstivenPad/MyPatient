3e74a4d883d536f6ddeafa67930657320331368a
EstivenPad <estivenps26@gmail.com>
2024-06-06 21:03:14 -0400

    feat: modifying layout and references

--- MyPatient.Web/Program.cs
+++ MyPatient.Web/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using MyPatient.DataAccess.DataContext;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<DatabaseContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 
 var app = builder.Build();
 
