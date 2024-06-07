489b1a69ec953050a92418e2625161a07e37de2d
EstivenPad <estivenps26@gmail.com>
2024-06-06 20:54:46 -0400

    feat(patient): adding DatabaseContext

---
+++ MyPatient.DataAccess/DataContext/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyPatient.Models;
+
+namespace MyPatient.DataAccess.DataContext
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Patient> Patients { get; set; }
+    }
+}
