5a0a164176d48238e796a93d4282ff18e5a1a6b0
EstivenPad <estivenps26@gmail.com>
2024-06-13 16:29:13 -0400

    feat(patient): overriding OnModelCreating

--- MyPatient.DataAccess/DataContext/DatabaseContext.cs
+++ MyPatient.DataAccess/DataContext/DatabaseContext.cs
@@ -10,5 +10,23 @@ namespace MyPatient.DataAccess.DataContext
         }
 
         public DbSet<Patient> Patients { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Patient>().HasData(
+                new Patient
+                {
+                    Id = 1,
+                    Record = "1234",
+                    Name = "Jose Reyes",
+                    Weight = 145.3,
+                    Age = 25,
+                    Identification = "402-1234567-1",
+                    Sex = false,
+                    IsInsured = true,
+                    ARS = "SeNaSa",
+                    NSS = "1234"
+                });
+        }
     }
 }
