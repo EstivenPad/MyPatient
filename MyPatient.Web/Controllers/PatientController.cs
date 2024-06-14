dd0e71e5d6f78ce35d275d6c325a2f8ada9aa265
EstivenPad <estivenps26@gmail.com>
2024-06-13 20:05:27 -0400

    feat: adding Toastr notification

--- MyPatient.Web/Controllers/PatientController.cs
+++ MyPatient.Web/Controllers/PatientController.cs
@@ -1,13 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using MyPatient.Web.Models;
+using MyPatient.Application.Services.Patient;
+using MyPatient.Models;
 
 namespace MyPatient.Web.Controllers
 {
     public class PatientController : Controller
     {
-        public IActionResult Index()
+        private readonly IPatientService _patientService;
+        public PatientController(IPatientService patientService)
         {
-            return View();
+            _patientService = patientService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Patient> patients = await _patientService.GetAllPatients();
+            return View(patients);
         }
 
         public IActionResult Create()
@@ -16,14 +24,87 @@ namespace MyPatient.Web.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(Patient patient)
+        public async Task<IActionResult> Create(Patient patient)
         {
             if (ModelState.IsValid)
             {
+                await _patientService.AddPatient(patient);
+
+                TempData["success"] = "Paciente creado correctamente.";
+
                 return RedirectToAction("Index");
             }
 
             return View(patient);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Patient patient = await _patientService.GetPatient(p => p.Id == id);
+
+            if (patient == null)
+            {
+                return NotFound();
+            }
+
+            return View(patient);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Patient patient)
+        {
+            if (ModelState.IsValid)
+            {
+                await _patientService.UpdatePatient(patient);
+
+                TempData["success"] = "Paciente actualizado correctamente.";
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Patient patient = await _patientService.GetPatient(p => p.Id == id);
+
+            if (patient == null)
+            {
+                return NotFound();
+            }
+
+            return View(patient);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeletePost(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Patient? patient = await _patientService.GetPatient(p => p.Id == id);
+
+            if (patient is null)
+                return NotFound();
+
+            await _patientService.RemovePatient(patient);
+
+            TempData["success"] = "Paciente eliminado correctamente.";
+
+            return RedirectToAction("Index");
         }
     }
 }
