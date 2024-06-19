01df8dc28bbde3ffcd8e0b83a976ed683a3dec50
Estiven Padilla Santos <estivenps26@gmail.com>
2024-06-19 11:26:29 -0400

    Feature patient module (#2)
    * feat(patient): adding 'Patient' model
    
    * feat: modifying Bootstrap CSS
    
    * feat(patient): adding PatientController
    
    * feat(patient): designing Create view
    
    * feat: implementing N-Tier Architecture
    
    * feat(patient): adding DatabaseContext
    
    * feat: modifying layout and references
    
    * feat(patient): designing Index view
    
    * feat(patient): implementing Repository pattern
    
    * feat(patient): implementing N-Tiers architecture
    
    * feat(patient): overriding OnModelCreating
    
    * feat(patient): adding Patient Service
    
    * feat: adding Toastr notification
    
    * feat(patient): adding filter repository method
    
    * feat(patient): adding patient services
    
    * feat(patient): adding patient filter action
    
    * feat(patient): designing Index patient view
    
    * feat(patient): redesigning Create view
    
    * feat(patient): designing Edit view
    
    * feat(patient): designing Delete view
    
    * feat(patient): registering services to the container

---
+++ MyPatient.DataAccess/Repository/BaseRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyPatient.DataAccess.DataContext;
+using MyPatient.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPatient.DataAccess.Repository
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        protected readonly DatabaseContext _context;
+        public BaseRepository(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Create(T entity)
+        {
+            _context.Set<T>().Add(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<T> Get(Expression<Func<T, bool>> filter)
+        {
+            return await _context.Set<T>().AsNoTracking().FirstOrDefaultAsync(filter);
+        }
+
+        public async Task<List<T>> GetAll()
+        {
+            return await _context.Set<T>().AsNoTracking().ToListAsync();
+        }
+
+        public async Task Update(T entity)
+        {
+            _context.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ MyPatient.Models/Patient.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyPatient.Models
+{
+    public class Patient
+    {
+        public long Id { get; set; }
+
+        [Required(ErrorMessage = "El Número de Record es requerido.")]
+        public string Record { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "La Cédula es requerida.")]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[- ]?([0-9]{7})[- ]?([0-9]{1})$", ErrorMessage = "La Cédula no es válida.")]
+        [MaxLength(13, ErrorMessage = "La Cédula debe tener 11 digitos.")]
+        public string Identification { get; set; } = string.Empty;
+        
+        [Required(ErrorMessage = "El Nombre es requerido.")]
+        public string Name { get; set; } = string.Empty;
+        
+        public double? Weight { get; set; }
+        
+        public int? Age { get; set; }
+        
+        public bool Sex { get; set; } = true;
+        
+        public bool IsInsured { get; set; } = false;
+        
+        public string? ARS { get; set; }
+        
+        public string? NSS { get; set; }
+
+        //TODO: ADD MA REFERENCE...
+    }
+}
