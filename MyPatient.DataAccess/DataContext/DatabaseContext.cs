28e70c80d8c7927b2804875a19b1ba17018a6bfc
Estiven Padilla Santos <estivenps26@gmail.com>
2024-06-18 22:03:05 -0400

    Feat/patient module
    * feat(patient): adding 'Patient' model
    
    * feat: modifying Bootstrap CSS
    
    * feat(patient): adding PatientController
    
    * feat(patient): designing Create view
    
    * feat: implementing N-Tier Architecture
    
    * feat(patient): adding DatabaseContext
    
    * feat: modifying layout and references
    
    * feat(patient): designing Index view
    
    * feat(patient): implementing Repository pattern
    
    * feat(patient): implementing N-Tiers architecture
    
    * feat(patient): overriding OnModelCreating
    
    * feat(patient): adding Patient Service
    
    * feat: adding Toastr notification
    
    * feat(patient): adding filter repository method
    
    * feat(patient): adding patient services
    
    * feat(patient): adding patient filter action
    
    * feat(patient): designing Index patient view
    
    * feat(patient): redesigning Create view
    
    * feat(patient): designing Edit view
    
    * feat(patient): designing Delete view
    
    * feat(patient): registering services to the container

---
+++ MyPatient.DataAccess/DataContext/DatabaseContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyPatient.Models;
+
+namespace MyPatient.DataAccess.DataContext
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Patient> Patients { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Patient>().HasData(
+                new Patient
+                {
+                    Id = 1,
+                    Record = "1234",
+                    Name = "Jose Reyes",
+                    Weight = 145.3,
+                    Age = 25,
+                    Identification = "402-1234567-1",
+                    Sex = false,
+                    IsInsured = true,
+                    ARS = "SeNaSa",
+                    NSS = "1234"
+                });
+        }
+    }
+}
---
+++ MyPatient.DataAccess/Repository/IRepository/IPatientRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyPatient.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyPatient.DataAccess.Repository.IRepository
+{
+    public interface IPatientRepository : IBaseRepository<Patient>
+    {
+        public Task<List<Patient>> GetAllFiltered(Expression<Func<Patient, bool>> expression);
+    }
+}
