0a7d83d7c40c57f4f669ed276365851d0ab0ba03
EstivenPad <estivenps26@gmail.com>
2024-06-27 11:22:27 -0400

    feat(MA): adding IMARepository and modifying others

--- MyPatient.DataAccess/Repository/IRepository/IMARepository.cs
+++ MyPatient.DataAccess/Repository/IRepository/IMARepository.cs
@@ -1,15 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
-using MyPatient.Models;
+﻿using MyPatient.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MyPatient.DataAccess.Repository.IRepository
 {
-    public interface IPatientRepository : IBaseRepository<Patient>
+    public interface IMARepository : IBaseRepository<MA>
     {
     }
 }
