3283e26cc0481f5c851af0cd7d159fc4a594efda
EstivenPad <estivenps26@gmail.com>
2024-06-27 11:20:55 -0400

    feat: adding MA repository and modifying others

--- MyPatient.DataAccess/DataContext/DatabaseContext.cs
+++ MyPatient.DataAccess/DataContext/DatabaseContext.cs
@@ -10,6 +10,8 @@ namespace MyPatient.DataAccess.DataContext
         }
 
         public DbSet<Patient> Patients { get; set; }
+        public DbSet<MA> MAs { get; set; }
+        //public DbSet<MedicalOrder> MedicalOrder { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -18,14 +20,26 @@ namespace MyPatient.DataAccess.DataContext
                 {
                     Id = 1,
                     Record = "1234",
-                    Name = "Jose Reyes",
+                    Name = "Guillermo Reyes",
                     Weight = 145.3,
                     Age = 25,
                     Identification = "402-1234567-1",
                     Sex = false,
                     IsInsured = true,
                     ARS = "SeNaSa",
-                    NSS = "1234"
+                    NSS = "1234",
+                    MAId = 1
+                });
+
+            modelBuilder.Entity<MA>().HasData(
+                new MA
+                {
+                    Id = 1,
+                    FirstName = "Miguel",
+                    LastName = "Tejada",
+                    Sex = false,
+                    Identification = "402-1234567-0",
+                    Exequatur = "1536-23"
                 });
         }
     }
--- MyPatient.DataAccess/Repository/MARepository.cs
+++ MyPatient.DataAccess/Repository/MARepository.cs
@@ -1,19 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
-using MyPatient.DataAccess.DataContext;
+﻿using MyPatient.DataAccess.DataContext;
 using MyPatient.DataAccess.Repository.IRepository;
 using MyPatient.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MyPatient.DataAccess.Repository
 {
-    public class PatientRepository : BaseRepository<Patient>, IPatientRepository
+    public class MARepository : BaseRepository<MA>, IMARepository
     {
-        public PatientRepository(DatabaseContext context) : base(context) 
+        public MARepository(DatabaseContext context) : base(context)
         {
         }
     }
